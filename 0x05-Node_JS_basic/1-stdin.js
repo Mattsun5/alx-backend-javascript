72d346b86e9a4fdfbbc84a12ab84a532f73b8d08
Mattsun5 <sundaymatt703@gmail.com>
2024-05-02 22:04:23 +0200

    javascript backend

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('welcome to Holberton School, what is your name?\n', (name) => {
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
 	console.log(`your name is: ${name}`);
 	rl.on('SIGINT', () => {
 		console.log('This important software is now closing\n');
