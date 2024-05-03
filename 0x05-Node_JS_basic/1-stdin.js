f3791c0b3e42e183cd1f2629c0e201e4941796a2
Mattsun5 <sundaymatt703@gmail.com>
2024-05-03 05:12:56 +0200

    node js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
-process.stdout.write('what is your name\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 if (process.stdin.isTTY) {
 	process.stdin.on('data', (data) => {
-		process.stdout.write(`your name: ${data.toString()}`);
+		process.stdout.write(`Your name is: ${data.toString()}`);
 		process.exit();
 	});
 } else {
 	process.stdin.on('data', (data) => {
-		process.stdout.write(`your name 2: ${data.toString()}`);
+		process.stdout.write(`Your name is: ${data.toString()}`);
 		process.exit();
 	});
 	process.on('exit', () => {
-		process.stdout.write('ended');
+		process.stdout.write('This important software is now closing\n');
 	});
 }
