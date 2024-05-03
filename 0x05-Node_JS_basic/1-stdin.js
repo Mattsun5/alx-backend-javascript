ad7a7dda8154373b09490b7af15aa9945435b921
Mattsun5 <sundaymatt703@gmail.com>
2024-05-03 05:05:36 +0200

    node js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
-const readline = require('node:readline');
+process.stdout.write('what is your name\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-	console.log(`your name is: ${name}`);
-	rl.on('SIGINT', () => {
-		console.log('This important software is now closing\n');
-		rl.close();
+if (process.stdin.isTTY) {
+	process.stdin.on('data', (data) => {
+		process.stdout.write(`your name: ${data.toString()}`);
+		process.exit();
 	});
-});
+} else {
+	process.stdin.on('data', (data) => {
+		process.stdout.write(`your name 2: ${data.toString()}`);
+		process.exit();
+	});
+	process.on('exit', () => {
+		process.stdout.write('ended');
+	});
+}
